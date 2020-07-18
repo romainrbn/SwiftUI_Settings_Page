0110a7ce713375bd7786485f3a4d27b899de3c00
Romain Rabouan <48293357+romainrbn@users.noreply.github.com>
2020-07-18 15:01:25 +0200

    Initial Commit

---
+++ Interaction/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Interaction
+//
+//  Created by Romain Rabouan on 18/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
